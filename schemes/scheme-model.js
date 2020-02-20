fc283db1ba36a1a0b7ba27a12a584799bccaa4db
adamwinzdesign <adamwinzdesign@gmail.com>
2020-02-19 22:54:34 -0600

    added list all steps for a given scheme

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -3,7 +3,7 @@ const schemeDB = require('../data/db-config.js');
 module.exports = {
   find,
   findById,
-  // findSteps,
+  findSteps,
   // add,
   // addStep,
   // update,
@@ -19,4 +19,15 @@ function findById(id) {
   return schemeDB('schemes')
     .where({ id })
     .first()
+}
+
+// list all steps for a scheme
+// schemes has id and scheme_name
+// steps has id, step_number, instructions, and scheme_id
+function findSteps(id) {
+  return schemeDB('steps')
+    .join('schemes', 'schemes.id', 'steps.scheme_id')
+    .select('steps.id', 'schemes.scheme_name', 'steps.step_number', 'steps.instructions')
+    .where('scheme_id', id)
+    .orderBy('steps.step_number')
 }
